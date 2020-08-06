715c55ea2050246c4efec4deed100c3c470fbe3d
unknown <javacloudmc22@iiht.tech>
2020-08-06 09:43:03 +0530

    Api gateway

---
+++ src/main/java/com/example/springbootzuulgatwayproxy/filters/ErrorFilter.java
@@ -0,0 +1,11 @@
+package com.example.springbootzuulgatwayproxy.filters;
+
+
+
+import org.springframework.cloud.netflix.zuul.ZuulServletFilter;
+
+public class ErrorFilter extends ZuulServletFilter {
+
+
+
+}
